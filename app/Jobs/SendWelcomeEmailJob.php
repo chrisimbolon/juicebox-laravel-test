e86275828e6b3f3157abdae3b9604b32a61cf926
Christyan Simbolon <sambungdata@gmail.com>
2025-11-27 13:00:36 +0700

    feat: create SendWelcomeEmail queued job for user registration

---
+++ app/Jobs/SendWelcomeEmailJob.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class SendWelcomeEmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function handle()
+    {
+        Log::info('Welcome email queued for: ' . $this->user->email);
+        Log::info('User registered: ' . $this->user->name);
+    }
+}
